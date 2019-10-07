bef0dd230844a9519c07828ca646ff7597bc1edc
Omar Khairy <omarkhairy21@gmail.com>
2019-10-08 00:52:03 +0200

    update action creator

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -5,7 +5,7 @@ import {
 
 export const fetchUser = () => async dispatch => {
 
-  const res = await axios.get('api/current_user');
+  const res = await axios.get('/api/current_user');
   dispatch({
     type: FETCH_USER,
     payload: res.data
